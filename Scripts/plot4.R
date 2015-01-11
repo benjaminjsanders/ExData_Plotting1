318c1ab113ca0da2fab60a1ecca2812db457d591
Benjamin Sanders <benjaminjsanders@yahoo.com>
2015-01-11 12:53:09 -0600

    I had forgotten to save my final changes

--- Scripts/plot4.R
+++ Scripts/plot4.R
@@ -9,12 +9,15 @@ data$datetime <- strptime(paste(as.character(data$Date),as.character(data$Time),
 
 
 # Create plot
-png(filename="plot4.png", height=480, width=480, bg = "transparent", mfrow=c(2,2))
-plot(data$datetime, data$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l")
+png(filename="plot4.png", height=480, width=480, bg = "transparent")
+par(mfrow=c(2,2))
+plot(data$datetime, data$Global_active_power, ylab="Global Active Power", xlab="", type="l")
+plot(data$datetime, data$Voltage, ylab="Voltage", xlab="datetime", type="l")
 plot(data$datetime, data$Sub_metering_1, ylab="Energy sub metering", xlab="", type="l")
 lines(data$datetime, data$Sub_metering_2, col="red")
 lines(data$datetime, data$Sub_metering_3, col="blue")
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1, col=c("black","red", "blue"))
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1, col=c("black","red", "blue"), bty = "n")
+plot(data$datetime, data$Global_reactive_power, ylab="Global_reactive_power", xlab="datetime", type="l")
 dev.off()
 
 # cleanup
